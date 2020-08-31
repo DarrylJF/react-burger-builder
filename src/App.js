7d42cebfb1c1b14be912f108cf5f0a36f97382b7
DarrylJF <33667200+DarrylJF@users.noreply.github.com>
2020-08-31 14:53:23 +0100

    project folder commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Layout from './components/Layout/Layout';
+import './App.scss';
+
+function App() {
+  return (
+    <div>
+      <Layout>
+        <p>Test</p>
+      </Layout>
+    </div>
+  );
+}
+
+export default App;
