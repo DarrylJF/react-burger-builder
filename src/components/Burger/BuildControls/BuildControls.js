017427b5343b1a489811ffbf7b055de355c94ae4
DarrylJF <33667200+DarrylJF@users.noreply.github.com>
2020-09-01 18:24:20 +0100

    BuildControls OrderButton styling added

--- src/components/Burger/BuildControls/BuildControls.js
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -18,8 +18,9 @@ const buildControls = (props) => (
               label={ctrl.label}
               added={() => props.ingredientAdded(ctrl.type)}
               removed={() => props.ingredientsRemoved(ctrl.type)}
-              disabled={props.disabled[ctrl.type]}/>
+              disabled={props.disabled[ctrl.type]} />
         ))}
+        <button className={classes.OrderButton}>ORDER NOW</button>
     </div>
 );
 
