b34e931e15c4c6eca934393b3b97d1fa8356cc11
DarrylJF <33667200+DarrylJF@users.noreply.github.com>
2020-09-04 00:13:49 +0100

    side bar toggle added on mobile viewports

--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -2,16 +2,26 @@ import React from 'react';
 import Logo from '../../Logo/Logo';
 import NavigationItems from '../NavigationItems/NavigationItems';
 import classes from './SideDrawer.module.scss';
+import BackDrop from '../../UI/Backdrop/Backdrop';
+import Auxillary from '../../../hoc/Auxillary';
 
-const sideDrawer = () => {
-  // ... 
+const sideDrawer = (props) => {
+  let attachedClasses = [classes.SideDrawer, classes.Close];
+  if (props.open) {
+    attachedClasses = [classes.SideDrawer, classes.Open];
+  }
   return (
-    <div className={classes.SideDrawer}>
-      <Logo/>
-      <nav>
-        <NavigationItems />
-      </nav>
-    </div>
+    <Auxillary>
+      <BackDrop show={props.open} clicked={props.closed}/>
+      <div className={attachedClasses.join(' ')}>
+        <div className={classes.Logo}>
+          <Logo />
+        </div>
+        <nav>
+          <NavigationItems />
+        </nav>
+      </div>
+    </Auxillary>
   )
 }
 
